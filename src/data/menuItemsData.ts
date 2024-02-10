dad7f1024181f8f2126e904a74f937a445637134
ManuelDenisDev <manueldenis.developer@gmail.com>
2024-02-10 13:28:35 +0100

    Componetizando el sitio y revisado el error

---
+++ src/data/menuItemsData.ts
@@ -0,0 +1,30 @@
+import type {MenuItem} from '../types/types';
+
+const menuItems: MenuItem[] = [
+	{
+		id: 1,
+		name: 'Experiencia',
+		url: '/experiencia',
+		decoration: 'decoration-purple-500',
+	},
+	{
+		id: 2,
+		name: 'Proyectos',
+		url: '/proyectos',
+		decoration: 'decoration-pink-600',
+	},
+	{
+		id: 3,
+		name: 'Sobre mí',
+		url: '/sobre-mi',
+		decoration: 'decoration-orange-400',
+	},
+	{
+		id: 4,
+		name: 'Contacto',
+		url: '/contacto',
+		decoration: 'decoration-cyan-500',
+	},
+];
+
+export default menuItems;
